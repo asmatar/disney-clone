c3bd6bd54b3fe625e979d80570f16e6e8755c8bd
asmatar <deruelle.arthur@gmail.com>
2021-05-29 09:42:44 +0200

    nav menu

---
+++ src/components/Header.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Header() {
+    return (
+        <Nav>
+            <Logo src='/images/logo.svg' />
+            <NavMenu>
+                <a>
+                    <img src="/images/home-icon.svg" alt="" />
+                    <span>HOME</span>
+                </a>
+                <a>
+                    <img src="/images/search-icon.svg" alt="" />
+                    <span>SEARCH</span>
+                </a>
+                <a>
+                    <img src="/images/watchlist-icon.svg" alt="" />
+                    <span>WATCHLIST</span>
+                </a>
+                <a>
+                    <img src="/images/original-icon.svg" alt="" />
+                    <span>ORIGINALS</span>
+                </a>
+                <a>
+                    <img src="/images/movie-icon.svg" alt="" />
+                    <span>MOVIES</span>
+                </a>
+                <a>
+                    <img src="/images/series-icon.svg" alt="" />
+                    <span>SERIES</span>
+                </a>
+            </NavMenu>
+            <UserImg src='/images/arthur.jpg' />
+        </Nav>
+    )
+}
+
+export default Header
+
+const Nav = styled.nav`
+height: 70 px;
+background : #090b13;
+display: flex;
+align-items: center;
+padding: 0 36px;
+`
+const Logo = styled.img`
+width: 80px;
+`
+const NavMenu = styled.div`
+    display: flex;
+    flex: 1;
+    margin-left: 25px;
+    align-items: center;
+    
+    a {
+        display: flex;
+        align-items: center;
+        padding: 0 12px; 
+        cursor: pointer;
+
+        img {
+            height: 20px;
+        }
+        
+        span {
+            font-size: 13px;
+            letter-spacing: 1.42px;
+        }
+    }
+`
+
+const UserImg = styled.img`
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+    cursor: pointer;
+`
