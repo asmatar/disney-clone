e4a61453f4e9c76944e29cde44d1fedade5c32f6
asmatar <deruelle.arthur@gmail.com>
2021-05-29 10:11:34 +0200

    home background css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
 function Home() {
     return (
-        <div>
+        <Container>
             Home
-        </div>
+        </Container>
     )
 }
 
 export default Home
+
+const Container = styled.main`
+    min-height: calc(100vh - 70px);
+    padding: 0 calc(3.5wh + 5px);
+    position: relative;
+
+    &:before {
+        background: url('/images/home-background.png') center center / cover no-repeat fixed;
+        content: '';
+        position: absolute;
+        // to cover all the page, all the div
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        // z-index to not cover the content of the div
+        z-index: -1;
+    }
+`
